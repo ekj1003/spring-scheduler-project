d20f010ca9aedf857e33310f7c5371755f6768cb
Eunkyo <jeongeunkyo3@gmail.com>
2024-08-14 11:03:51 +0900

    🩹 fix : password field 자료형 String으로 수정

--- schedule.sql
+++ schedule.sql
@@ -3,7 +3,7 @@ create table schedule
     id       bigint       not null auto_increment,
     contents varchar(500) not null,
     manager varchar(255) not null,
-    password bigint not null,
+    password varchar(200) not null,
     createTime timestamp not null,
     modifiedTime timestamp not null,
     primary key (id)
