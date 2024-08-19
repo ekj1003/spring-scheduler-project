c87eb15270ccc3f70783af662fbec6ebaa0987c4
Eunkyo <jeongeunkyo3@gmail.com>
2024-08-20 00:36:43 +0900

    ✨ update : JPA 환경 설정 및, schedule entity 수정

--- src/main/java/com/sparta/scheduler/entity/Schedule.java
+++ src/main/java/com/sparta/scheduler/entity/Schedule.java
@@ -1,6 +1,8 @@
 package com.sparta.scheduler.entity;
 
 import com.sparta.scheduler.dto.ScheduleRequestDto;
+import jakarta.persistence.*;
+import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,17 +10,30 @@ import lombok.Setter;
 import java.util.Date;
 import java.time.LocalDateTime;
 
+@Entity
 @Getter
 @Setter
+@Table(name="schedule")
 @NoArgsConstructor
 public class Schedule {
     //할일, 담당자명, 비밀번호, 작성/수정일
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name="contents", nullable = false, length = 500)
     private String contents;
+
+    @Column(name="manager", nullable = false)
     private String manager;
+
+    @Column(name="password", nullable = false, length = 200)
     private String password;
+
+    @Column(name="createTime", nullable = false)
     private LocalDateTime createTime;
+
+    @Column(name="modifiedTime", nullable = false)
     private LocalDateTime modifiedTime;
 
     public Schedule(ScheduleRequestDto requestDto) {
