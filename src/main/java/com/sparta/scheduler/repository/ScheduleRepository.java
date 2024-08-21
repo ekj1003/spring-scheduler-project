f6c11d132c7331194f0ab3aaf9fe03dd26709bb8
Eunkyo <jeongeunkyo3@gmail.com>
2024-08-21 11:06:46 +0900

    🩹 fix : 안쓰는 method 삭제

--- src/main/java/com/sparta/scheduler/repository/ScheduleRepository.java
+++ src/main/java/com/sparta/scheduler/repository/ScheduleRepository.java
@@ -12,17 +12,7 @@ import java.util.stream.Collectors;
 // ScheduleRepository.java
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
     Optional<Schedule> findById(Long id);
-
     List<Schedule> findByModifiedDateBetween(LocalDateTime start, LocalDateTime end);
-
     List<Schedule> findByManager(String manager);
     List<Schedule> findByManagerAndModifiedDateBetween(String manager, LocalDateTime start, LocalDateTime end);
-    default List<Schedule> findByModifiedDateAndManager(LocalDate modifiedDate, String manager) {
-        LocalDateTime start = modifiedDate.atStartOfDay();  // 시작 시간
-        LocalDateTime end = start.plusDays(1).minusNanos(1);  // 끝 시간
-        List<Schedule> schedules = findByModifiedDateBetween(start, end);
-        return schedules.stream()
-                .filter(schedule -> manager.equals(schedule.getManager()))
-                .collect(Collectors.toList());
-    }
 }
