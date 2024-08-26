37202e918307e2cf30bc1241cfc4911ada8c9f26
Eunkyo <jeongeunkyo3@gmail.com>
2024-08-26 13:42:15 +0900

    ✨update : 2-3. 댓글 필드(댓글 내용, 작성일, 수정일, 작성 유저명)

---
+++ src/main/java/com/sparta/scheduler/entity/Comment.java
@@ -0,0 +1,48 @@
+package com.sparta.scheduler.entity;
+
+import com.sparta.scheduler.dto.CommentRequestDto;
+import com.sparta.scheduler.dto.ScheduleRequestDto;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name="comment")
+@NoArgsConstructor
+public class Comment extends Timestamped{
+
+    // Schedule : Comment 1대 다 관계
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 작성 유저명
+    @Column(name="writer", nullable = false)
+    private String writer;
+
+    // 댓글 내용
+    @Column(name="comments", nullable = false, length = 500)
+    private String comments;
+
+
+    @ManyToOne
+    @JoinColumn(name = "schedule_id", insertable = false, updatable = false)
+    private Schedule schedule;
+
+
+    public Comment(CommentRequestDto requestDto) {
+        this.writer = requestDto.getWriter();
+        this.comments = requestDto.getComments();
+    }
+
+    public void update(CommentRequestDto requestDto) {
+        this.writer = requestDto.getWriter();
+        this.comments = requestDto.getComments();
+    }
+
+
+}
