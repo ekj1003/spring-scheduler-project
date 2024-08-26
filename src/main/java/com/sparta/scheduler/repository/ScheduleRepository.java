67032be59daa61eabf0f6dcce9aac6ebf759a431
Eunkyo <jeongeunkyo3@gmail.com>
2024-08-26 11:53:16 +0900

    🩹 fix : 1-1. schedule 저장, 단건 조회, 수정

--- src/main/java/com/sparta/scheduler/repository/ScheduleRepository.java
+++ src/main/java/com/sparta/scheduler/repository/ScheduleRepository.java
@@ -10,7 +10,8 @@ import java.util.Optional;
 // ScheduleRepository.java
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
     Optional<Schedule> findById(Long id);
-    List<Schedule> findByModifiedDateBetween(LocalDateTime start, LocalDateTime end);
-    List<Schedule> findByManager(String manager);
-    List<Schedule> findByManagerAndModifiedDateBetween(String manager, LocalDateTime start, LocalDateTime end);
+//    List<Schedule> findByWriter(String writer);
+//    List<Schedule> findByTitle(String Title);
+//    List<Schedule> findByModifiedDateBetween(LocalDateTime start, LocalDateTime end);
+//    List<Schedule> findByManagerAndModifiedDateBetween(String manager, LocalDateTime start, LocalDateTime end);
 }
--- src/main/java/com/sparta/scheduler/service/ScheduleService.java
+++ src/main/java/com/sparta/scheduler/service/ScheduleService.java
@@ -7,8 +7,6 @@ import com.sparta.scheduler.repository.ScheduleRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDate;
-import java.util.List;
 import java.util.NoSuchElementException;
 
 @Service
@@ -18,7 +16,7 @@ public class ScheduleService {
         this.scheduleRepository = scheduleRepository;
     }
 
-
+    // 1-1. 일정 저장
     public ScheduleResponseDto createSchedule(ScheduleRequestDto requestDto) {
 
         Schedule schedule = new Schedule(requestDto);
@@ -31,6 +29,7 @@ public class ScheduleService {
         return scheduleResponseDto;
     }
 
+    // 1-1. 일정 단건 조회
     public ScheduleResponseDto getOneSchedule(Long id) {
         // DB 조회 (비밀번호 필드를 제외)
         Schedule schedule = scheduleRepository.findById(id)
@@ -39,58 +38,49 @@ public class ScheduleService {
         return new ScheduleResponseDto(schedule);
     }
 
-    public List<ScheduleResponseDto> getSchedules(LocalDate modifiedDate, String manager) {
-        List<Schedule> schedules;
-        if (modifiedDate != null && manager != null) {
-            schedules = scheduleRepository.findByManagerAndModifiedDateBetween(manager, modifiedDate.atStartOfDay(), modifiedDate.atStartOfDay().plusDays(1).minusNanos(1));
-        }
-        else if (modifiedDate != null) {
-            schedules = scheduleRepository.findByModifiedDateBetween(modifiedDate.atStartOfDay(), modifiedDate.atStartOfDay().plusDays(1).minusNanos(1));
-        }
-        else if (manager != null) {
-            schedules = scheduleRepository.findByManager(manager);
-        }
-        else {
-            schedules = scheduleRepository.findAll();
-        }
-        return schedules.stream().map(ScheduleResponseDto::new).toList();
-    }
 
-
+    // 1-1. 일정 수정
     @Transactional
-    public ScheduleResponseDto updateSchedule(Long id, String password, ScheduleRequestDto requestDto) {
+    public ScheduleResponseDto updateSchedule(Long id, ScheduleRequestDto requestDto) {
 
         // 해당 스케줄이 DB에 존재하는지 확인
         Schedule schedule = findSchedule(id);
-        if(password.equals(schedule.getPassword())) {
-            // schedule 내용 수정
-            schedule.update(requestDto);
+        schedule.update(requestDto);
 
-            return new ScheduleResponseDto(schedule);
-        } else {
-            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
-        }
+        return new ScheduleResponseDto(schedule);
     }
 
-
-    public Long deleteSchedule(Long id, String password) {
+    // 1-1. 일정 삭제
+    public Long deleteSchedule(Long id) {
 
         // 해당 스케줄이 DB에 존재하는지 확인
         Schedule schedule = findSchedule(id);
-        if(password.equals(schedule.getPassword())) {
-            // schedule 삭제
-            scheduleRepository.delete(schedule);
+        scheduleRepository.delete(schedule);
 
-            return id;
-        } else {
-            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
-        }
+        return id;
     }
 
     private Schedule findSchedule(Long id) {
         return scheduleRepository.findById(id).orElseThrow(() ->
                 new IllegalArgumentException("선택한 메도는 존재하지 않습니다. "));
     }
+
+//    public List<ScheduleResponseDto> getSchedules(LocalDate modifiedDate, String manager) {
+//        List<Schedule> schedules;
+//        if (modifiedDate != null && manager != null) {
+//            schedules = scheduleRepository.findByManagerAndModifiedDateBetween(manager, modifiedDate.atStartOfDay(), modifiedDate.atStartOfDay().plusDays(1).minusNanos(1));
+//        }
+//        else if (modifiedDate != null) {
+//            schedules = scheduleRepository.findByModifiedDateBetween(modifiedDate.atStartOfDay(), modifiedDate.atStartOfDay().plusDays(1).minusNanos(1));
+//        }
+//        else if (manager != null) {
+//            schedules = scheduleRepository.findByManager(manager);
+//        }
+//        else {
+//            schedules = scheduleRepository.findAll();
+//        }
+//        return schedules.stream().map(ScheduleResponseDto::new).toList();
+//    }
 
 
 }
