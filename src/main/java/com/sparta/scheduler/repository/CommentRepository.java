0fc6ffe9a08342ad3f4ec49ca9f99272aacb5e61
Eunkyo <jeongeunkyo3@gmail.com>
2024-08-26 16:20:35 +0900

    ✨update : 2-2. 댓글 저장, 단건 조회 기능 구현 완료

---
+++ src/main/java/com/sparta/scheduler/repository/CommentRepository.java
@@ -0,0 +1,13 @@
+package com.sparta.scheduler.repository;
+
+import com.sparta.scheduler.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    Optional<Comment> findById(Long id);
+}
